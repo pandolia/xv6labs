56d141c779b42d654649e78120f6ce64ac3ad51f
Pandolia <pandolia@yeah.net>
2024-08-14 14:45:49 +0800

    complete lab5

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -47,8 +47,14 @@ sys_sbrk(void)
   if(argint(0, &n) < 0)
     return -1;
   addr = myproc()->sz;
-  if(growproc(n) < 0)
-    return -1;
+  if(n < 0)
+  {
+    if(addr + n < 0) return -1;
+    else uvmdealloc(myproc()->pagetable, myproc()->sz, myproc()->sz+n);  
+  }
+  // if(growproc(n) < 0)
+  //   return -1;
+  myproc()->sz += n;
   return addr;
 }
 
